e3506506eb2878923900a70463f08eb71adff63d
lappet69 <sinabariba.andre@gmail.com>
2022-02-02 16:32:47 +0700

    update contact about sidemenu

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,171 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./contact.scss";
+import instagram from "../../components/assets/instagram.svg";
+import $ from 'jquery'
 
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      message: "",
-    };
+
+
+const Contact = ()=>{
+  const [data,setData] =useState()
+
+ const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://type.fit/api/quotes",
+    "method": "GET"
   }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    axios({
-      method: "POST",
-      url: "http://localhost:3002/send",
-      data: this.state,
-    }).then((response) => {
-      if (response.data.status === "success") {
-        alert("Message Sent.");
-        this.resetForm();
-      } else if (response.data.status === "fail") {
-        alert("Message failed to send.");
-      }
-    });
+  
+  $.ajax(settings).done(function (response) {
+    const data = JSON.parse(response);
+    setData(data)
+  });
+  
+  const randomQuote=()=>{
+    const num =()=>{
+      Math.random(1,1600)
+    }
   }
-
-  resetForm() {
-    this.setState({ name: "", email: "", message: "" });
-  }
-
-  render() {
+randomQuote()
+  
     return (
       <div className="content">
-        <section id="contact" className="contact">
-          <div
-            className="container aos-init aos-animate justify-content-center"
-            data-aos="fade-up"
-          >
-            <div className="section-title">
-              <h2>Contact</h2>
-              <p>
-                Magnam dolores commodi suscipit. Necessitatibus eius consequatur
-                ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
-                quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
-              </p>
-            </div>
-
-            <div
-              className="row aos-init aos-animate"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
-              <div className="col-md">
-                <div className="row">
-                  <div className="col-md-12">
-                    <div className="info-box">
-                      <i className="bx bx-map"></i>
-                      <h3>Our Address</h3>
-                      <p>A108 Adam Street, New York, NY 535022</p>
-                    </div>
-                  </div>
-                  <div className="col-md">
-                    <div className="info-box mt-4">
-                      {/* <i className="bx bx-envelope"></i> */}
-                      <h3>Email Us</h3>
-                      <p>
-                        info@example.com
-                        <br />
-                        contact@example.com
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="info-box mt-4">
-                      <i className="bx bx-phone-call"></i>
-                      <h3>Call Us</h3>
-                      <p>
-                        +1 5589 55488 55
-                        <br />
-                        +1 6678 254445 41
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-md-6">
-                <form
-                  action=""
-                  method="post"
-                  // role="form"
-                  className="php-email-form"
-                >
-                  <div className="">
-                    <div className="col form-group">
-                      <input
-                        type="text"
-                        name="name"
-                        className="form-control"
-                        id="name"
-                        placeholder="Your Name"
-                        required=""
-                      />
-                    </div>
-                    <div className="col form-group">
-                      <input
-                        type="email"
-                        className="form-control"
-                        name="email"
-                        id="email"
-                        placeholder="Your Email"
-                        required=""
-                      />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="subject"
-                      id="subject"
-                      placeholder="Subject"
-                      required=""
-                    />
-                  </div>
-                  <div className="form-group">
-                    <textarea
-                      className="form-control"
-                      name="message"
-                      rows="5"
-                      placeholder="Message"
-                      required=""
-                    ></textarea>
-                  </div>
-                  <div className="my-3">
-                    <div className="loading">Loading</div>
-                    <div className="error-message"></div>
-                    <div className="sent-message">
-                      Your message has been sent. Thank you!
-                    </div>
-                  </div>
-                  <div className="text-center">
-                    <button type="submit">Send Message</button>
-                  </div>
-                </form>
-              </div>
-            </div>
+        <div className="contact-us">
+          <h2 className="text-contact">LOGO</h2>
+          <h3 className="text-company">PT Desain Revolusi Indonesia </h3>
+          <p className="text-information">Contact us</p>
+          <div className="row sosmed">
+            <div className="col-md-6 sosmed-wa">WA</div>
+            <div className="col-md-6 sosmed-email">EMAIL</div>
+            <div className="col-md-6 sosmed-ig">IG</div>
+            <div className="col-md-6 sosmed-fb">Facebook</div>
           </div>
-        </section>
+          <figure className="text-center">
+            <blockquote className="blockquote">
+              <p>A well-known quote, contained in a blockquote element.</p>
+            </blockquote>
+            <figcaption className="blockquote-footer">
+              Someone famous in <cite title="Source Title">Source Title</cite>
+            </figcaption>
+          </figure>
+        </div>
       </div>
     );
   }
 
-  onNameChange(event) {
-    this.setState({ name: event.target.value });
-  }
 
-  onEmailChange(event) {
-    this.setState({ email: event.target.value });
-  }
 
-  onMessageChange(event) {
-    this.setState({ message: event.target.value });
-  }
-}
 
 export default Contact;
--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Jumbotron from "../../components/jumbotron/Jumbotron";
 import "./landingpage.scss";
 
 import heroimg2 from "../../components/assets/bg-3.jpg";
 import heroimg1 from "../../components/assets/bg-2.jpg";
 import { Carousel } from "react-bootstrap";
+import axios from "axios";
+
 
 const LandingPage = () => {
+  
+  
   return (
     <div className="content">
       <div className="row d-flex">
